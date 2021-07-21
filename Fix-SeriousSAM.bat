f03ede000c732e4462db630a2eb4da660f9e00ae
Cody Dostal <cody@dostal.co>
2021-07-21 08:39:59 -0400

    Add comments

--- Fix-SeriousSAM.bat
+++ Fix-SeriousSAM.bat
@@ -1,3 +1,6 @@
+REM Change the permissions of all files in C:\Windows\System32\config\ according to Microsoft guidance: https://msrc.microsoft.com/update-guide/vulnerability/CVE-2021-36934
 icacls %windir%\system32\config\*.* /inheritance:e
+REM Delete all volume shadow copies to remove existing vulnerable files
 vssadmin delete shadows /all /quiet
+REM Create a new system restore point that is titled as today's date.
 wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "%DATE%",100,1
